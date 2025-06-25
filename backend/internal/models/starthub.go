c24ab7936f79a495f8f8a67f87f8cd058429102b
Cetin Erdem <cetoerdem1@gmail.com>
2025-06-25 23:28:28 +0400

    delete unnecessary comment

--- backend/internal/models/starthub.go
+++ backend/internal/models/starthub.go
@@ -3,15 +3,15 @@ package models
 import "time"
 
 type StartHub struct {
-	ID                     string    `json:"id"` // Changed to string for UUID
+	ID                     string    `json:"id"`
 	Name                   string    `json:"name"`
 	Description            string    `json:"description"`
 	Location               string    `json:"location"`
 	TeamSize               int       `json:"team_size"`
 	URL                    string    `json:"url"`
 	Email                  string    `json:"email"`
-	JoinDate               time.Time `json:"join_date"`            // Changed to time.Time
-	Categories             []string  `json:"categories,omitempty"` // Will be populated from join
+	JoinDate               time.Time `json:"join_date"`
+	Categories             []string  `json:"categories,omitempty"`
 	CollaboratingStarthubs []string  `json:"collaborating_starthubs,omitempty"`
 	ExternalCollaborators  []string  `json:"external_collaborators,omitempty"`
 }
@@ -24,5 +24,5 @@ type CreateStartHubRequest struct {
 	TeamSize    int      `json:"team_size"`
 	URL         string   `json:"url"`
 	Email       string   `json:"email" validate:"required,email"`
-	Categories  []string `json:"categories"` // Category names to associate
+	Categories  []string `json:"categories"`
 }
