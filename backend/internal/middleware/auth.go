b625abf7dc3f34909927092e01da405b937255a0
Cetin Erdem <cetoerdem1@gmail.com>
2025-06-26 13:23:01 +0400

    middleware for require JWT

---
+++ backend/internal/middleware/auth.go
@@ -0,0 +1,44 @@
+package middleware
+
+import (
+	"strings"
+
+	"github.com/ecetinerdem/starthub-backend/pkg/utils"
+	"github.com/gofiber/fiber/v2"
+)
+
+func RequireAuth(c *fiber.Ctx) error {
+	authHeader := c.Get("Authorization")
+
+	if authHeader == "" {
+		return c.Status(fiber.StatusUnauthorized).JSON(fibe.Map{
+			"error": "Authorization header required",
+		})
+	}
+
+	//Bearer
+	tokenParts := strings.Split(authHeader, " ")
+	if len(tokenParts) != 2 || tokenParts[0] != "Bearer" {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"error": "Invalid authorization format. Use: Bearer <token>",
+		})
+	}
+
+	token := tokenParts[1]
+
+	//Validate JWT
+
+	claims, err := utils.ValidateJWT(token)
+
+	if err != nil {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"error": "Invalid or expired token",
+		})
+	}
+
+	c.Locals("user_id", claims.UserID)
+	c.Locals("user_email", claims.Email)
+	c.Locals("user_role", claims.Role)
+
+	return c.Next()
+}
